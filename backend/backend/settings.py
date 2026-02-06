f830053e182067ddeb12c234dbb208b1cec386b7
SHIJU A <118278088+shijuvtm@users.noreply.github.com>
2026-02-06 14:15:57 +0530

    Update ALLOWED_HOSTS in settings.py
    Restrict ALLOWED_HOSTS to specific domains and localhost.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,8 +31,12 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["*", ".onrender.com"]
 
+ALLOWED_HOSTS = [
+    "jobconnect-1ofu.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 
 # Application definition
 
