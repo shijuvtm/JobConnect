e2769c1738b91e13fcc57c9343b3fc59c0af1eb9
SHIJU A <118278088+shijuvtm@users.noreply.github.com>
2026-03-17 15:31:11 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,7 +83,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOWED_ORIGINS = [
+    "https://job-connect-black-ten.vercel.app",
+]
+
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
