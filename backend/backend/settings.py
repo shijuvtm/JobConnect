8d0c6652e83b78128d8304f261972b50d86cdff4
SHIJU A <118278088+shijuvtm@users.noreply.github.com>
2026-03-20 11:38:45 +0530

    Update settings.py
    Update smtp local to Bravio email

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -116,13 +116,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
-
+"""
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("email")
 EMAIL_HOST_PASSWORD = os.environ.get("password")
+"""
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
@@ -177,3 +178,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Add this line
+
